28e0e26e80330fe1d61f6dff26b6952c5a39fda8
kingshill <kingshill69696969@gmail.com>
2021-05-18 04:05:28 -0400

    fix: Change deploy script oracle to testnet

--- contracts/scripts/deploy-script.js
+++ contracts/scripts/deploy-script.js
@@ -14,7 +14,7 @@ async function main() {
   // We get the contract to deploy
   const Prediction = await ethers.getContractFactory('MockBNBPricePrediction');
   const prediction = await Prediction.deploy(
-    "0x0567F2323251f0Aab15c8dFb1967E4e8A7D42aeE", //_oracle BNB / USD, 8 DEC
+    "0x2514895c72f50D8bd4B4F9b1110F0D6bD2c97526", //_oracle BNB / USD, 8 DEC, mainnet: 0x0567F2323251f0Aab15c8dFb1967E4e8A7D42aeE
     adminAddress,                                 //_adminAddress
     operatorAddress,                              //_operatorAddress
     "100",                                        //_intervalBlocks
