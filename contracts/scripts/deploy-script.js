79330227ea12839f1233c474d6b00b3d9ab3bd2b
kingshill <kingshill69696969@gmail.com>
2021-05-18 11:48:40 -0400

    fix: Change deploy script to use non-mock contract

--- contracts/scripts/deploy-script.js
+++ contracts/scripts/deploy-script.js
@@ -12,7 +12,7 @@ async function main() {
   adminAddress = await admin.getAddress();
   operatorAddress = await operator.getAddress();
   // We get the contract to deploy
-  const Prediction = await ethers.getContractFactory('MockBNBPricePrediction');
+  const Prediction = await ethers.getContractFactory('BNBPricePrediction');
   const prediction = await Prediction.deploy(
     "0x2514895c72f50D8bd4B4F9b1110F0D6bD2c97526", //_oracle BNB / USD, 8 DEC, mainnet: 0x0567F2323251f0Aab15c8dFb1967E4e8A7D42aeE
     adminAddress,                                 //_adminAddress
